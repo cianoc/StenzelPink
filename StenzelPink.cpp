1e1157f75e3da74a9c75c28fd73476ca1154fb60
Nathan Ho <nathan@snappizz.com>
2016-05-30 22:55:14 -0700

    license

--- StenzelPink.cpp
+++ StenzelPink.cpp
@@ -1,3 +1,22 @@
+/*
+
+    StenzelPink SuperCollider UGen
+    Copyright (C) 2016 Nathan Ho.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
 #include "SC_PlugIn.hpp"
 #include "pink.h"
 
@@ -31,7 +50,7 @@ private:
         }
     }
 
-    // Otherwise, gotta write to a size-16 buffer and
+    // Otherwise, gotta write to a danged size-16 buffer, track phase and all that
     void next(int inNumSamples) {
         for (int i = 0; i < inNumSamples; i += 1) {
             if (mPhase == 0) {
